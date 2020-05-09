dba0a45d4d00990d7d2907baa2b0818a36e44f83
Michael <mike@mikangali.com>
2020-05-09 02:01:14 +0200

    Update home title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header"
 export default function Home() {
   return (
     <div style={{ color: `purple`}}>
-      <Header headerText="Hello Gatsby !" level="1" />
+      <Header headerText="My Lucid Dream" level="1" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />   
     </div>
