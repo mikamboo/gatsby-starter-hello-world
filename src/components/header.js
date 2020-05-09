ad53ecaea33754d9e008ea6893bc0b60be1beee9
aymardhb9 <ian.otando@gmail.com>
2020-05-09 17:31:58 +0200

    [IOT] : Change contact menu label

--- src/components/header.js
+++ src/components/header.js
@@ -6,20 +6,19 @@ export default function Header(props) {
     <p>
       <Link to="/">Home</Link> | &nbsp;
       <Link to="/about">About Page</Link> | &nbsp;
-      <Link to="/contact">Contact Page</Link>
-    </p>  
-  );
+      <Link to="/contact">Contact Us</Link>
+    </p>
+  )
 
   const titlef = (level, text) => {
     if (level === "1") {
       return <h1>{text}</h1>
     }
-    if(props.level === "2") {
+    if (props.level === "2") {
       return <h2>{text}</h2>
-    } 
+    }
     return <h3>{text}</h3>
   }
 
   return [menu, titlef(props.level, props.headerText)]
-
 }
