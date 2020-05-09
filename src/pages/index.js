74a567c679105c24d18227af065734007ebeb951
Michael <mike@mikangali.com>
2020-05-09 17:06:41 +0200

    Fix Lin errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: `purple`}}>
-      <Header headerText="My Lucid Dream" level="1" />
+      <Header headerText="My Lucid Dream" level="2" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />   
     </div>
