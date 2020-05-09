51277143a5ef64ad9b6ffae07cb2beb5bbe65668
Michaël P.O <mike.pambo@gmail.com>
2020-05-09 17:25:37 +0200

    Update home title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple`}}>
-      <Header headerText="My Lucid Dream" level="2" />
+    <div style={{ color: `purple` }}>
+      <Header headerText="My Lucid Dream :)" level="2" />
       <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />   
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
 }
